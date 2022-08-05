ef929315dbc3472e58a9d9b6da31b9db8ac4d692
Yollanda Lima <yollandalima123@gmail.com>
2022-08-05 11:30:46 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+// = serve para atribuir valor a uma variavel
+// == serve para comparar valor de uma variavel com outra
+// != serve para comparar valor de uma variavel com outra
+// >  serve para comparar valor de uma variavel com outra
+// <  serve para comparar valor de uma variavel com outra
+// >= serve para comparar valor de uma variavel com outra
+// <= serve para comparar valor de uma variavel com outra
+// && serve para comparar valor de uma variavel com outra
+// || serve para comparar valor de uma variavel com outra
+// ! serve para inverter o valor de uma variavel
+// ^ serve para comparar valor de uma variavel com outra
+// & serve para comparar valor de uma variavel com outra
+// | serve para comparar valor de uma variavel com outra
+// + serve para somar valor de uma variavel com outra
+// - serve para subtrair valor de uma variavel com outra
+// * serve para multiplicar valor de uma variavel com outra
+// / serve para dividir valor de uma variavel com outra
+// % serve para obter o resto de uma divisao
+// ++ serve para incrementar valor de uma variavel
+// -- serve para decrementar valor de uma variavel
+// += serve para somar valor de uma variavel com outra e atribuir a variavel
+// -= serve para subtrair valor de uma variavel com outra e atribuir a variavel
+// *= serve para multiplicar valor de uma variavel com outra e atribuir a variavel
+// /= serve para dividir valor de uma variavel com outra e atribuir a variavel
+// %= serve para obter o resto de uma divisao e atribuir a variavel
+// ? serve para fazer uma condicao
+// : serve para fazer uma condicao
+
+Console.Write("Hello World!");
+
+int i;
+int i2 = i = 10;
+i2--;
+
+i = i2 == 42 ? 10 : 20; //se i2 for 42, i recebe 10, se nao, i recebe 20
+
+Console.WriteLine(i);
